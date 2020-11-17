c5db845565e75ad8c8fe2cbbf62a6ddce1c049a5
Rafael Rocha Lopez <rafael9764@hotmail.com>
2020-11-17 13:09:14 -0400

    creacion de componentes de registrar vendedor

---
+++ ClientApp/src/app/registrar-vendedor/registrar-vendedor-form/registrar-vendedor-form.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-registrar-vendedor-form',
+  templateUrl: './registrar-vendedor-form.component.html',
+  styles: [
+  ]
+})
+export class RegistrarVendedorFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
