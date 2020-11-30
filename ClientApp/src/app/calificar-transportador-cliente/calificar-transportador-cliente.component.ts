f619c3e200ecbfb14992c7ed043b5f488ab3e1c3
educato-gonzales <clau.yeri@gmail.com>
2020-11-30 14:43:25 -0800

    creacion modulos para calificar

---
+++ ClientApp/src/app/calificar-transportador-cliente/calificar-transportador-cliente.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calificar-transportador-cliente',
+  templateUrl: './calificar-transportador-cliente.component.html',
+  styles: [
+  ]
+})
+export class CalificarTransportadorClienteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ ClientApp/src/app/calificar-transportador-vendedor/calificar-transportador-vendedor.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calificar-transportador-vendedor',
+  templateUrl: './calificar-transportador-vendedor.component.html',
+  styles: [
+  ]
+})
+export class CalificarTransportadorVendedorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
